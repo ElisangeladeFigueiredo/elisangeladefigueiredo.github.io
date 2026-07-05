3892d8a1d666119fd82ff4f07c0d3467f968a7ec
ELISANGELA\elisangela <defigueiredoelisangela@gmail.com>
2026-07-05 20:03:01 -0300

    Ajustes no layout e adiçinando redes sociais

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ if (toggle && nav) {
 }
 const year = document.querySelector('#year');
 if (year) year.textContent = new Date().getFullYear();
+
+const idMail = "btn-email-";
+const idMsg = "email-copy-msg-";
+
+for(let i = 1; i <= 2; i++) {
+  let _emailBtn = document.getElementById(idMail + i);
+  let _emailMsg = document.getElementById(idMsg + i);
+
+  _emailBtn.addEventListener("click", async () => {
+    let _email = 'elisangeladefigueiredo@adv.oabsp.org.br';
+
+    try {
+      await navigator.clipboard.writeText(_email);
+
+      _emailMsg.textContent = "'elisangeladefigueiredo@adv.oabsp.org.br' copiado para a área de transferência.";
+
+      setTimeout(() => {
+        _emailMsg.textContent = "";
+      }, 3000);
+    } catch (error) {
+      _emailMsg.textContent = "E-mail: " + _email;
+    }
+
+    // Não usamos preventDefault().
+    // Assim o mailto continua funcionando normalmente.
+  });
+}
